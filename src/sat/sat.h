502c6dcee16598d92ce1ea1ad8189f5794f256d0
Trevor Hansen <thansen@csse.unimelb.edu.au>
2009-09-04 13:56:44 +0000

    Header file I meant to checkin with #180

---
+++ src/sat/sat.h
@@ -0,0 +1,20 @@
+#ifndef SAT_H_
+#define SAT_H_
+
+#include "core/Solver.h"
+#include "core/SolverTypes.h"
+//#include "../sat/simp/SimpSolver.h"
+//#include "../sat/unsound/UnsoundSimpSolver.h"
+
+
+#endif /* SAT_H_ */
+#ifndef SAT_H_
+#define SAT_H_
+
+#include "core/Solver.h"
+#include "core/SolverTypes.h"
+//#include "../sat/simp/SimpSolver.h"
+//#include "../sat/unsound/UnsoundSimpSolver.h"
+
+
+#endif /* SAT_H_ */
