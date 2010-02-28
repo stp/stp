bba695b85c450cc9e1e1cfbb6c51dc09d10dbd38
Trevor Hansen <thansen@csse.unimelb.edu.au>
2010-02-28 11:29:00 +0000

    Testcase and temporary bugfix for an interface problem reported by Alvin Cheung.

---
+++ tests/c-api-tests/interface-check.c
@@ -0,0 +1,24 @@
+// Bug reported by Alvin Cheung. Thanks.
+
+#include "c_interface.h"
+#include <iostream>
+#include <cassert>
+#include <stdexcept>
+
+int main(int argc, char * argv [])
+{
+  ::VC vc = vc_createValidityChecker();
+  ::Expr b1 = ::vc_trueExpr(vc);
+  ::Expr b2 = ::vc_falseExpr(vc);
+  ::Expr andExpr = ::vc_andExpr(vc, b1, b2);
+
+  if(::getExprKind(andExpr) !=  ::AND )
+	throw new std::runtime_error("sadfas");
+
+  ::Expr simplifiedExpr = ::vc_simplify(vc, andExpr);
+
+	if (getExprKind(simplifiedExpr) !=  ::FALSE )
+	throw new std::runtime_error("sa22dfas");
+  return 0;
+}
+
