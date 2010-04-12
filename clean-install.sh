e27f8e30797d64752932dc15dd7c991f05adccea
Stephen McCamant <mccamant@cs.umn.edu>
2010-04-12 19:36:49 +0000

    Add configuration options for selecting SAT solver, plus some related cleanups to how scripts and Makefiles include and invoke each other. Patch from Peter Collingbourne.

--- clean-install.sh
+++ clean-install.sh
@@ -1,23 +1,8 @@
 #!/bin/sh
 PREFIX=$HOME
 
-while [ $# -gt 0 ]; do
-    case "$1" in
-	--with-prefix=*)
-	    arg=`expr "x$1" : 'x[^=]*=\(.*\)'`
-	    PREFIX=$arg;;
-	*)
-	    echo "Usage: ./clean-install [options]"
-	    echo "   --with-prefix=/prefix/path   Installs STP at the specified path"
-	    echo "configure failed"
-	    exit 1;;
-    esac
-
-    shift
-done
-
 make configclean
-./scripts/configure --with-prefix=$PREFIX
+. scripts/configure
 make clean
 make install
 
