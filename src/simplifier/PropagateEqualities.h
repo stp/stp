9327bfdb37c901a603ebe2d339a3625c66508898
Trevor Hansen <thansen@csse.unimelb.edu.au>
2011-12-29 02:55:10 +0000

    Refactor. automatically layout code.

--- src/simplifier/PropagateEqualities.h
+++ src/simplifier/PropagateEqualities.h
@@ -4,12 +4,11 @@
 #include "../AST/AST.h"
 #include "../STPManager/STPManager.h"
 
-    //This finds conjuncts which are one of: (= SYMBOL BVCONST), (= BVCONST (READ SYMBOL BVCONST)),
-    // (IFF SYMBOL TRUE), (IFF SYMBOL FALSE), (IFF SYMBOL SYMBOL), (=SYMBOL SYMBOL)
-    // or (=SYMBOL BVCONST).
-    // It tries to remove the conjunct, storing it in the substitutionmap. It replaces it in the
-    // formula by true.
-
+//This finds conjuncts which are one of: (= SYMBOL BVCONST), (= BVCONST (READ SYMBOL BVCONST)),
+// (IFF SYMBOL TRUE), (IFF SYMBOL FALSE), (IFF SYMBOL SYMBOL), (=SYMBOL SYMBOL)
+// or (=SYMBOL BVCONST).
+// It tries to remove the conjunct, storing it in the substitutionmap. It replaces it in the
+// formula by true.
 
 namespace BEEV
 {
