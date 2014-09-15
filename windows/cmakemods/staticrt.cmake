821facb5036c3b7bd781f14323c02d771dedae76
Dan Liew <daniel.liew@imperial.ac.uk>
2014-09-16 00:02:50 +0100

    Remove some old windows stuff that looks dead.

--- windows/cmakemods/staticrt.cmake
+++
@@ -1,6 +0,0 @@
-# version 1.00
-# to make msvc/gcc to link statically the runtime libraries  
-get_filename_component(MYMODESDIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-include("${MYMODESDIR}/msvcmt.cmake")
-include("${MYMODESDIR}/slibgcc.cmake")
-
