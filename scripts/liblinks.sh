94d7a9d895fb599972da4c03dfe2d3352da22c26
Dan Liew <daniel.liew@imperial.ac.uk>
2014-09-15 23:58:15 +0100

    Delete scripts that are no longer used.

--- scripts/liblinks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# You can use this script to symbolically link the requested libraries.  
-# You can then compile STP using:
-#   make STATIC=true
-# if you want to use the SMT-LIB parser
-ln -s `g++ -print-file-name=libstdc++.a`
-ln -s `g++ -print-file-name=libc.a`
