37acd230aea6d5c0e65213cf72c0d4b975c18d20
Dan Liew <daniel.liew@imperial.ac.uk>
2014-10-21 01:40:15 +0100

    Add .clang-format style file and handy script to run from project root to format all STP sources (apart from external projects) with clang-format.

---
+++ utils/clang-format.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Format headers and source files but avoid external projects
+files="$(find include/ -iname '*.h' -o -iname '*.cpp' | sort)"
+files="$files $(find lib/ \( -iname '*.h' -o -iname '*.cpp' \) -not \( -path 'lib/extlib-*' -o -path 'lib/Sat/minisat/*' -o -path 'lib/Sat/cryptominisat2/*' \) | sort )"
+files="$files $(find tests/api \( -iname '*.h' -o -iname '*.cpp' -o -iname '*.c' \) | sort)"
+for f in $files; do
+    echo "Formating $f"
+    clang-format -i -style=file $f
+done
