3998d4d4ba4a70645c88ffa9a3957e75fe224be7
Mate Soos <soos.mate@gmail.com>
2014-11-06 15:15:42 +0100

    Adding all missing licenses to files' headers
    Fixes #136

--- lib/AST/RunTimes.cpp
+++ lib/AST/RunTimes.cpp
@@ -1,8 +1,35 @@
+/********************************************************************
+ * AUTHORS: Trevor Hansen
+ *
+ * BEGIN DATE: November, 2005
+ *
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+********************************************************************/
+
+
 //Hold the runtime statistics. E.g. how long was spent in simplification.
 //The times don't add up to the runtime, because we allow multiple times to
 //be counted simultaneously. For example, the current Transform()s call
 //Simplify_TopLevel, so inside simplify time will be counted towards both
 //Simplify_TopLevel & Transform.
+
+
 
 // This is intended as a low overhead profiling class. So runtimes can
 // always be tracked.
