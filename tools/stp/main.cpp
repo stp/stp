1577b35397d9027e47999a809b140be5f43da2f3
Mate Soos <soos.mate@gmail.com>
2014-11-08 16:06:21 +0100

    Making an stp_simple executable for users without Boost program options

--- tools/stp/main.cpp
+++ tools/stp/main.cpp
@@ -1,4 +1,3 @@
-
 /********************************************************************
  * AUTHORS: Vijay Ganesh, Trevor Hansen
  *
@@ -23,26 +22,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ********************************************************************/
 
-#include "STPProgramGlobals.h"
-#include "stp/AST/AST.h"
-#include "stp/Printer/AssortedPrinters.h"
-#include "stp/Printer/printers.h"
-#include "stp/STPManager/STPManager.h"
-#include "stp/STPManager/STP.h"
-#include "stp/AST/NodeFactory/TypeChecker.h"
-#include "stp/AST/NodeFactory/SimplifyingNodeFactory.h"
-#include "stp/cpp_interface.h"
-#include <sys/time.h>
-#include <memory>
-// FIXME: External library
-#include "extlib-abc/cnf_short.h"
-#include "GitSHA1.h"
+#include "main_common.h"
 
 #include <boost/lexical_cast.hpp>
 #include <boost/program_options.hpp>
 using boost::lexical_cast;
 namespace po = boost::program_options;
-
 
 #ifdef EXT_HASH_MAP
 using namespace __gnu_cxx;
@@ -53,19 +38,10 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-extern int smtparse(void*);
-extern int smt2parse();
-extern int cvcparse(void*);
-extern int cvclex_destroy(void);
-extern int smtlex_destroy(void);
-extern int smt2lex_destroy(void);
-
 namespace BEEV {
 void setHardTimeout(int);
 }
 
-// Amount of memory to ask for at beginning of main.
-static const intptr_t INITIAL_MEMORY_PREALLOCATION_SIZE = 4000000;
 /********************************************************************
  * MAIN FUNCTION:
  *
@@ -77,24 +53,35 @@ static const intptr_t INITIAL_MEMORY_PREALLOCATION_SIZE = 4000000;
  * step 5. Call SAT to determine if input is SAT or UNSAT
 ********************************************************************/
 
-static STPMgr* bm;
-static bool onePrintBack = false;
+class ExtraMain: public Main
+{
+public:
+    void try_parsing_options(int argc
+        , char** argv
+        , po::variables_map& vm
+        , po::positional_options_description& pos_options
+    );
+    int create_and_parse_options(int argc, char** argv) override;
+    void create_options();
+    int parse_options(int argc, char** argv);
 
-//Files to read
-static string infile;
-static FILE* toClose;
-extern FILE* cvcin;
-extern FILE* smtin;
-extern FILE* smt2in;
+    po::options_description cmdline_options;
+    po::options_description visible_options;
+    po::positional_options_description pos_options;
+};
 
-//For options
-static size_t hardTimeout;
-static size_t random_seed;
-static po::options_description cmdline_options;
-static po::options_description visible_options;
-static po::positional_options_description pos_options;
+int ExtraMain::create_and_parse_options(int argc, char** argv)
+{
+    create_options();
+    int ret = parse_options(argc, argv);
+    if (ret != 0) {
+        return ret;
+    }
+    return 0;
+}
 
-void try_parsing_options(
+
+void ExtraMain::try_parsing_options(
     int argc
     , char** argv
     , po::variables_map& vm
@@ -160,14 +147,13 @@ void try_parsing_options(
     }
 }
 
-void create_options()
+void ExtraMain::create_options()
 {
     po::options_description hiddenOptions("Hidden options");
     hiddenOptions.add_options()
     ("file", po::value<string>(&infile)
         , "input file");
 
-    ParserBM = bm;
     // Declare the supported options.
     po::options_description general_options("Most important options");
     general_options.add_options()
@@ -288,7 +274,7 @@ void create_options()
     pos_options.add("file", 1);
 }
 
-int parse_options(int argc, char** argv)
+int ExtraMain::parse_options(int argc, char** argv)
 {
     po::variables_map vm;
     try_parsing_options(argc, argv, vm, pos_options);
@@ -421,233 +407,8 @@ int parse_options(int argc, char** argv)
     return 0;
 }
 
-void parse_file(ASTVec* AssertsQuery)
+int main(int argc, char **argv)
 {
-    TypeChecker nfTypeCheckSimp(*bm->defaultNodeFactory, *bm);
-    TypeChecker nfTypeCheckDefault(*bm->hashingNodeFactory, *bm);
-
-    Cpp_interface piTypeCheckSimp(*bm, &nfTypeCheckSimp);
-    Cpp_interface piTypeCheckDefault(*bm, &nfTypeCheckDefault);
-
-    // If you are converting formats, you probably don't want it simplifying (at least I dont).
-    if (onePrintBack) {
-        parserInterface = &piTypeCheckDefault;
-    } else {
-        parserInterface = &piTypeCheckSimp;
-    }
-
-    parserInterface->startup();
-
-    if (onePrintBack) {
-        if (bm->UserFlags.smtlib2_parser_flag) {
-            cerr << "Printback from SMTLIB2 inputs isn't currently working." << endl;
-            cerr << "Please try again later" << endl;
-            cerr << "It works prior to revision 1354" << endl;
-            exit(1);
-        }
-    }
-
-
-    if (bm->UserFlags.smtlib1_parser_flag) {
-        smtparse((void*) AssertsQuery);
-        smtlex_destroy();
-    } else if (bm->UserFlags.smtlib2_parser_flag) {
-        smt2parse();
-        smt2lex_destroy();
-    } else {
-        cvcparse((void*) AssertsQuery);
-        cvclex_destroy();
-    }
-    parserInterface = NULL;
-    if (toClose != NULL) {
-        fclose(toClose);
-    }
-}
-
-void print_back(ASTNode& query, ASTNode& asserts)
-{
-    ASTNode original_input = bm->CreateNode(AND, bm->CreateNode(NOT, query), asserts);
-
-    if (bm->UserFlags.print_STPinput_back_flag) {
-        if (bm->UserFlags.smtlib1_parser_flag) {
-            bm->UserFlags.print_STPinput_back_SMTLIB2_flag = true;
-        } else {
-            bm->UserFlags.print_STPinput_back_CVC_flag = true;
-        }
-    }
-
-    if (bm->UserFlags.print_STPinput_back_CVC_flag) {
-        //needs just the query. Reads the asserts out of the data structure.
-        print_STPInput_Back(original_input);
-    }
-
-    if (bm->UserFlags.print_STPinput_back_SMTLIB1_flag) {
-        printer::SMTLIB1_PrintBack(cout, original_input);
-    }
-
-    if (bm->UserFlags.print_STPinput_back_SMTLIB2_flag) {
-        printer::SMTLIB2_PrintBack(cout, original_input);
-    }
-
-    if (bm->UserFlags.print_STPinput_back_C_flag) {
-        printer::C_Print(cout, original_input);
-    }
-
-    if (bm->UserFlags.print_STPinput_back_GDL_flag) {
-        printer::GDL_Print(cout, original_input);
-    }
-
-    if (bm->UserFlags.print_STPinput_back_dot_flag) {
-        printer::Dot_Print(cout, original_input);
-    }
-}
-
-void read_file()
-{
-     bool error = false;
-     if (bm->UserFlags.smtlib1_parser_flag) {
-        smtin = fopen(infile.c_str(), "r");
-        toClose = smtin;
-        if (smtin == NULL) {
-            error = true;
-        }
-    } else if (bm->UserFlags.smtlib2_parser_flag) {
-        smt2in = fopen(infile.c_str(), "r");
-        toClose = smt2in;
-        if (smt2in == NULL) {
-            error = true;
-        }
-    }
-
-    else {
-        cvcin = fopen(infile.c_str(), "r");
-        toClose = cvcin;
-        if (cvcin == NULL) {
-            error = true;
-        }
-    }
-
-    if (error)
-    {
-        std::string errorMsg("Cannot open ");
-        errorMsg += infile;
-        FatalError(errorMsg.c_str());
-    }
-}
-
-static void errorHandler(const char* error_msg)
-{
-    cerr << prog << ": Error: " << error_msg << endl;
-    exit(-1);
-}
-
-int main(int argc, char** argv)
-{
-    // Register the error handler
-    vc_error_hdlr = errorHandler;
-
-#if  !defined(__MINGW32__) && !defined(__MINGW64__) && !defined( _MSC_VER)
-    // Grab some memory from the OS upfront to reduce system time when
-    // individual hash tables are being allocated
-    if (sbrk(INITIAL_MEMORY_PREALLOCATION_SIZE) == ((void*) - 1)) {
-        FatalError("Initial allocation of memory failed.");
-    }
-#endif /* !defined(__MINGW32__) && !defined(__MINGW64__) && !defined( _MSC_VER) */
-
-
-    bm = new STPMgr();
-    toClose = NULL;
-
-    auto_ptr<SimplifyingNodeFactory> simplifyingNF( new SimplifyingNodeFactory(*bm->hashingNodeFactory, *bm));
-    bm->defaultNodeFactory = simplifyingNF.get();
-
-    // The simplified keeps a pointer to whatever is set as the default node factory.
-    Simplifier* simp  = new Simplifier(bm);
-    auto_ptr<Simplifier> simpCleaner(simp);
-
-    ArrayTransformer* arrayTransformer = new ArrayTransformer(bm, simp);
-    auto_ptr<ArrayTransformer> atClearner(arrayTransformer);
-
-    ToSAT* tosat = new ToSAT(bm);
-    auto_ptr<ToSAT> tosatCleaner(tosat);
-
-    AbsRefine_CounterExample* Ctr_Example = new AbsRefine_CounterExample(bm, simp, arrayTransformer);
-    auto_ptr<AbsRefine_CounterExample> ctrCleaner(Ctr_Example);
-
-    create_options();
-    int ret = parse_options(argc, argv);
-    if (ret != 0) {
-        return ret;
-    }
-
-    GlobalSTP = new STP(bm, simp, arrayTransformer, tosat, Ctr_Example);
-
-    // If we're not reading the file from stdin.
-    if (!infile.empty()) {
-        read_file();
-    }
-
-    //want to print the output always from the commandline.
-    bm->UserFlags.print_output_flag = true;
-    ASTVec* AssertsQuery = new ASTVec;
-
-    bm->GetRunTimes()->start(RunTimes::Parsing);
-    parse_file(AssertsQuery);
-    bm->GetRunTimes()->stop(RunTimes::Parsing);
-
-    /*  The SMTLIB2 has a command language. The parser calls all the functions,
-     *  so when we get to here the parser has already called "exit". i.e. if the
-     *  language is smt2 then all the work has already been done, and all we need
-     *  to do is cleanup...
-     *    */
-    if (!bm->UserFlags.smtlib2_parser_flag) {
-
-        if (((ASTVec*) AssertsQuery)->empty()) {
-            FatalError("Input is Empty. Please enter some asserts and query\n");
-        }
-
-        if (((ASTVec*) AssertsQuery)->size() != 2) {
-            FatalError("Input must contain a query\n");
-        }
-
-        ASTNode asserts = (*(ASTVec*) AssertsQuery)[0];
-        ASTNode query = (*(ASTVec*) AssertsQuery)[1];
-
-        if (onePrintBack) {
-            print_back(query, asserts);
-            return 0;
-        }
-
-        SOLVER_RETURN_TYPE ret = GlobalSTP->TopLevelSTP(asserts, query);
-        if (bm->UserFlags.quick_statistics_flag) {
-            bm->GetRunTimes()->print();
-        }
-        (GlobalSTP->tosat)->PrintOutput(ret);
-
-        asserts = ASTNode();
-        query = ASTNode();
-    }
-
-    //Without cleanup
-    if (bm->UserFlags.isSet("fast-exit", "1")) {
-        exit(0);
-    }
-
-    //Propery tidy up
-    AssertsQuery->clear();
-    delete AssertsQuery;
-
-    _empty_ASTVec.clear();
-
-    simpCleaner.release();
-    atClearner.release();
-    tosatCleaner.release();
-    ctrCleaner.release();
-
-    delete GlobalSTP;
-    delete ParserBM;
-
-    Cnf_ClearMemory();
-
-    return 0;
+    ExtraMain main;
+    return main.main(argc, argv);
 }
---
+++ tools/stp/main_common.h
@@ -0,0 +1,82 @@
+/********************************************************************
+ * AUTHORS: Vijay Ganesh, Trevor Hansen, Mate Soos
+ *
+ * BEGIN DATE: November, 2005
+ *
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+********************************************************************/
+
+#ifndef __MAIN_COMMON_H__
+#define __MAIN_COMMON_H__
+
+#include <stdio.h>
+#include "STPProgramGlobals.h"
+#include "stp/AST/AST.h"
+#include "stp/Printer/AssortedPrinters.h"
+#include "stp/Printer/printers.h"
+#include "stp/STPManager/STPManager.h"
+#include "stp/STPManager/STP.h"
+#include "stp/AST/NodeFactory/TypeChecker.h"
+#include "stp/AST/NodeFactory/SimplifyingNodeFactory.h"
+#include "stp/cpp_interface.h"
+#include <sys/time.h>
+#include <memory>
+#include <string>
+#include <stdio.h>
+#include "extlib-abc/cnf_short.h"
+#include "GitSHA1.h"
+
+extern int smtparse(void*);
+extern int smt2parse();
+extern int cvcparse(void*);
+extern int cvclex_destroy(void);
+extern int smtlex_destroy(void);
+extern int smt2lex_destroy(void);
+extern void errorHandler(const char* error_msg);
+
+// Amount of memory to ask for at beginning of main.
+extern const intptr_t INITIAL_MEMORY_PREALLOCATION_SIZE;
+
+class Main {
+public:
+    Main();
+    virtual ~Main();
+    int main(int argc, char** argv);
+    void parse_file(ASTVec* AssertsQuery);
+    void print_back(ASTNode& query, ASTNode& asserts);
+    void read_file();
+
+    STPMgr* bm;
+    bool onePrintBack;
+
+    virtual int create_and_parse_options(int argc, char** argv);
+
+    //Files to read
+    std::string infile;
+    FILE* toClose;
+    FILE* cvcin;
+    FILE* smtin;
+    FILE* smt2in;
+
+    //For options
+    size_t hardTimeout;
+    size_t random_seed;
+};
+
+#endif //__MAIN_COMMON_H__
