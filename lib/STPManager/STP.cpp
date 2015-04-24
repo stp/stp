stp/stp

commit 31914acc0db6e9a6ab466ffe271c1b46e97a0d31

Remvoing unmaintained CryptoMiniSat2
This removes one more dependency, especially one that is hard to
maintain and I don't intend to maintain. CMS4 is much more maintainable
and is much more mature. Should perform significantly better and eat way
less memory.

lib/STPManager/STP.cpp
@@ -28,9 +28,6 @@ THE SOFTWARE.
 #include "stp/Simplifier/constantBitP/ConstantBitPropagation.h"
 #include "stp/Simplifier/constantBitP/NodeToFixedBitsMap.h"
 
-
-#include "stp/Sat/CryptoMinisat.h"
-
 #ifdef USE_CRYPTOMINISAT4
 #include "stp/Sat/CryptoMinisat4.h"
 #endif
@@ -85,9 +82,6 @@ SATSolver* STP::get_new_sat_solver()
   {
     case UserDefinedFlags::SIMPLIFYING_MINISAT_SOLVER:
       newS = new SimplifyingMinisat;
-      break;
-    case UserDefinedFlags::CRYPTOMINISAT_SOLVER:
-      newS = new CryptoMinisat;
       break;
     case UserDefinedFlags::CRYPTOMINISAT4_SOLVER:
       #ifdef USE_CRYPTOMINISAT4

commit 7e1e3c6804e39279928293946035c4cc736c0b0e

Fix compilation (broken by 31914acc0db6e9a6ab466ffe271c1b46e97a0d31) when Cryptominisat4 was not available at configure time.

lib/STPManager/STP.cpp
@@ -87,9 +87,8 @@ SATSolver* STP::get_new_sat_solver()
       #ifdef USE_CRYPTOMINISAT4
       newS = new CryptoMinisat4;
       #else
-      std::cerr << "WARNING: Falling back to CryptoMiniSatv2 since v4 \
-            was not available at STP library compile time" << std::endl;
-      newS = new CryptoMinisat;
+      std::cerr << "CryptoMinisat4 support was not enabled at configure time." << std::endl;
+      exit(-1);
       #endif
       break;
     case UserDefinedFlags::MINISAT_SOLVER: