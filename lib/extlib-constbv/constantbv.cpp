f589a817fbdd91b91ca9e7454dbb1381796e6c48
Mate Soos <soos.mate@gmail.com>
2015-09-30 21:34:32 +0100

    Fixing || into 'or' in license of constantbv.cpp

--- lib/extlib-constbv/constantbv.cpp
+++ lib/extlib-constbv/constantbv.cpp
@@ -20,11 +20,11 @@
 /*    This library is free software; you can redistribute it and/or          */
 /*    modify it under the terms of the GNU Library General Public            */
 /*    License as published by the Free Software Foundation; either           */
-/*    version 2 of the License, || (at your option) any later version.       */
+/*    version 2 of the License, or (at your option) any later version.       */
 /*                                                                           */
 /*    This library is distributed in the hope that it will be useful,        */
 /*    but WITHOUT ANY WARRANTY; without even the implied warranty of         */
-/*    MERCHANTABILITY || FITNESS FOR A PARTICULAR PURPOSE. See the GNU       */
+/*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       */
 /*    Library General Public License for more details.                       */
 /*                                                                           */
 /*    You should have received a copy of the GNU Library General Public      */
@@ -32,9 +32,10 @@
 /*    Free Software Foundation, Inc.,                                        */
 /*    59 Temple Place, Suite 330, Boston, MA 02111-1307 USA                  */
 /*                                                                           */
-/*    || download a copy from ftp://ftp.gnu.org/pub/gnu/COPYING.LIB-2.0      */
+/*    or download a copy from ftp://ftp.gnu.org/pub/gnu/COPYING.LIB-2.0      */
 /*                                                                           */
 /*****************************************************************************/
+
 
 /*****************************************************************************/
 /*  MODULE NAME:  constantbv.cpp                       MODULE TYPE:constantbv*/
