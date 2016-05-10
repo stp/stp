7358f732a42dace4a9ce56047d2715b0724277fa
Mate Soos <soos.mate@gmail.com>
2016-05-10 23:24:30 +0100

    Making static compile a Release one

--- scripts/build/build_static.sh
+++ scripts/build/build_static.sh
@@ -4,5 +4,5 @@ set -x
 
 rm -rf stp* tests tools lib bindings include
 rm -rf cmake* CM* Makefile ./*.cmake
-cmake -DBUILD_SHARED_LIBS=OFF -DFORCE_CMS=ON -DONLY_SIMPLE=ON  ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DFORCE_CMS=ON -DONLY_SIMPLE=ON  ..
 make -j4
