bb6eeee61968f483ac21f9b3fee0b178f17a146e
Mate Soos <soos.mate@gmail.com>
2016-05-17 21:04:28 +0100

    Cannot run make check on static compile

--- scripts/build/build_static.sh
+++ scripts/build/build_static.sh
@@ -6,4 +6,3 @@ rm -rf stp* tests tools lib bindings include
 rm -rf cmake* CM* Makefile ./*.cmake
 cmake -DENABLE_TESTING=ON -DBUILD_STATIC_BIN=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DFORCE_CMS=ON   ..
 make -j4 VERBOSE=1
-make check
