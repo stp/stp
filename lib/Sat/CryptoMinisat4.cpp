30ab1a8216a945483adf631b410c7cf14ab9b677
Mate Soos <soos.mate@gmail.com>
2016-06-06 01:22:02 +0100

    Adding commented-out experimental code

--- lib/Sat/CryptoMinisat4.cpp
+++ lib/Sat/CryptoMinisat4.cpp
@@ -45,6 +45,7 @@ CryptoMinisat4::CryptoMinisat4(int num_threads)
   // s->log_to_file("stp.cnf");
   s->set_num_threads(num_threads);
   s->set_default_polarity(false);
+  //s->set_allow_otf_gauss();
   temp_cl = (void*)new vector<CMSat::Lit>;
 }
 
