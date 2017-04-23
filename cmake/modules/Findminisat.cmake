6b08d7618ebe4046c07bff60ed4d76253cfbee75
Mate Soos <soos.mate@gmail.com>
2017-04-23 18:53:42 +0100

    Debugging Windows build

--- cmake/modules/Findminisat.cmake
+++ cmake/modules/Findminisat.cmake
@@ -30,7 +30,7 @@ set(MINISAT_DEFINITIONS "")
 message(STATUS "Finding minisat headers...")
 message(STATUS "looking at:  ${MINISAT_INCLUDE_DIRS}")
 find_path(MINISAT_INCLUDE_DIR minisat/core/Solver.h
-          PATHS ${MINISAT_INCLUDE_DIRS}
+          HINTS ${MINISAT_INCLUDE_DIRS}
           PATH_SUFFIXES minisat minisat2 )
 message(STATUS "found: MINISAT_INCLUDE_DIR: ${MINISAT_INCLUDE_DIR}")
 
@@ -42,13 +42,13 @@ if (Minisat_USE_STATIC_LIBS)
       set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
   endif(UNIX AND WITH_MY_LIB_STATIC)
   find_library(MINISAT_LIBRARY NAMES libminisat.a libminisat2.a
-             PATHS ${MINISAT_LIBDIR} ${MINISAT_LIBRARY_DIRS} )
+             HINTS ${MINISAT_LIBDIR} ${MINISAT_LIBRARY_DIRS} )
   macro_pop_required_vars(CMAKE_FIND_LIBRARY_SUFFIXES)
 else()
   message(STATUS "Finding dynamic minisat libs...")
   message(STATUS "looking at:  ${MINISAT_LIBDIR}")
   find_library(MINISAT_LIBRARY NAMES minisat minisat2
-             PATHS ${MINISAT_LIBDIR} ${MINISAT_LIBDIR}\lib ${MINISAT_LIBRARY_DIRS} )
+             HINTS ${MINISAT_LIBDIR} ${MINISAT_LIBDIR}/lib ${MINISAT_LIBRARY_DIRS} )
  message(STATUS "found: MINISAT_LIBRARY: ${MINISAT_LIBRARY}")
 endif()
 
