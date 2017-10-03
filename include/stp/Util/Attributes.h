7681543682d33e4d42f3f29a6875a4ea2740534b
Mate Soos <soos.mate@gmail.com>
2017-10-03 23:18:03 +0100

    Trying to fix the thread-local storage for C

--- include/stp/Util/Attributes.h
+++ include/stp/Util/Attributes.h
@@ -57,11 +57,22 @@ THE SOFTWARE.
     #define DLL_LOCAL
 #endif
 
-// Provide thread safety if possible
+//Defining THREAD_LOCAL
 #if __cplusplus >= 201103L
     #define THREAD_LOCAL thread_local
+#elif defined _WIN32 && ( \
+       defined _MSC_VER || \
+       defined __ICL || \
+       defined __DMC__ || \
+       defined __BORLANDC__ )
+    #define THREAD_LOCAL __declspec(thread)
+/* note that ICC (linux) and Clang are covered by __GNUC__ */
+#elif defined __GNUC__ || \
+       defined __SUNPRO_C || \
+       defined __xlC__
+    #define THREAD_LOCAL __thread
 #else
-    #define THREAD_LOCAL
+  #error "Cannot define THREAD_LOCAL"
 #endif
 
-#endif
+#endif //ATTRIBUTES_H_
