0c983cf0811945caf7881b83924965d01e6ba90a
Andrew V. Jones <andrew.jones@vector.com>
2019-05-16 11:34:22 +0100

    Writing a Python interface timeout test

--- lib/Sat/CryptoMinisat5.cpp
+++ lib/Sat/CryptoMinisat5.cpp
@@ -103,7 +103,7 @@ bool CryptoMiniSat5::solve(bool& timeout_expired) // Search without assumptions.
   if (ret == CMSat::l_Undef)
   {
     timeout_expired = true;
-    // TODO: do we want an assertion on the cause of the timeout
+    // TODO: do we want an assertion on the case of a timeout
   }
   return ret == CMSat::l_True;
 }
