91423ad6cba0ad2b4a3f029348436ff529f1c0df
Andrew V. Jones <andrew.jones@vector.com>
2020-08-03 23:44:13 +0000

    Working towards using GitHub Actions for CI
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>

---
+++ contrib/setup-gtest.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+dep_dir="deps"
+install_dir=$(readlink -fm ${dep_dir}/install)
+
+[ ! -d "${install_dir}" ] && mkdir -p "${install_dir}"
+
+dep="gtest"
+
+cd ${dep_dir}
+git clone https://github.com/stp/googletest ${dep}
+cd ${dep}
+mkdir build && cd build
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX:PATH=${install_dir} ..
+cmake --build .
+sudo cmake --install .
+cd ..
+
+# EOF
