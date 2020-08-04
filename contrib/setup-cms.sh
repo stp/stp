1d8939385c4059809cc8cd7f913f721f49a40a65
Andrew V. Jones <andrew.jones@vector.com>
2020-08-04 09:27:37 +0000

    Clean-up of the GitHub Actions + the 'contrib' scripts
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>

--- contrib/setup-cms.sh
+++ contrib/setup-cms.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail
+set -e -u -o pipefail
 
 dep_dir="deps"
-install_dir=$(readlink -fm ${dep_dir}/install)
+install_dir=$(readlink -fm "${dep_dir}"/install)
 
 [ ! -d "${install_dir}" ] && mkdir -p "${install_dir}"
 
 dep="cms"
 
-cd ${dep_dir}
-git clone https://github.com/msoos/cryptominisat ${dep}
-cd ${dep}
+cd "${dep_dir}"
+git clone https://github.com/msoos/cryptominisat "${dep}"
+cd "${dep}"
 mkdir build && cd build
-cmake -G Ninja -DCMAKE_INSTALL_PREFIX:PATH=${install_dir} ..
-cmake --build .
-sudo cmake --install .
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${install_dir}" ..
+cmake --build . --parallel "$(nproc)"
+cmake --install .
 cd ..
 
 # EOF
