fa68837623bef43cd96a39c6520eedf26071c4a1
Trevor Hansen <trev_abroad@yahoo.com>
2022-03-10 14:50:52 +1100

    refactor. Order the user flags.

--- include/stp/STPManager/UserDefinedFlags.h
+++ include/stp/STPManager/UserDefinedFlags.h
@@ -40,30 +40,22 @@ struct UserDefinedFlags // not copyable
   UserDefinedFlags& operator=(UserDefinedFlags const&) = delete;
 
 public:
-  // collect statistics on certain functions
-  bool stats_flag = false;
+  /* Parsing options */
+  bool smtlib1_parser_flag = false;
+  bool smtlib2_parser_flag = false;
 
-  //collect and delete objects via interface.
+  /* collect and delete objects via interface. */
   bool cinterface_exprdelete_on_flag = true;
 
-  int64_t timeout_max_conflicts = -1;
+  /* Output details of how the solving went*/
+  bool stats_flag = false;
+  bool quick_statistics_flag = false;
 
-  int64_t timeout_max_time = -1; // seconds
-
-  // print DAG nodes
-  bool print_nodes_flag = false;
-
-  // the Simplifier functions (which might increase the size).
-  bool optimize_flag = true;
-  // turn on word level bitvector solver
-  bool wordlevel_solve_flag = true;
-  // Remove equalities.
-  bool propagate_equalities = true;
-  // Constant bit propagation enabled.
-  bool bitConstantProp_flag = true;
-  
-  // Number of iterations of AIG rewrites.
-  int64_t AIG_rewrites_iterations = 0;
+  /* Control simplification */
+  bool optimize_flag = true; // the Simplifier functions (which might increase the size).
+  bool wordlevel_solve_flag = true;   // turn on word level bitvector solver
+  bool propagate_equalities = true; // Remove equalities.
+  bool bitConstantProp_flag = true; // Constant bit propagation enabled.
   bool enable_unconstrained = true;
   bool enable_flatten = false;
   bool enable_ite_context = true;
@@ -71,28 +63,22 @@ public:
   bool enable_use_intervals = true;
   bool enable_pure_literals = true;
   bool enable_always_true = false;
+  int64_t AIG_rewrites_iterations = 0; // Number of iterations of AIG rewrites.
   int64_t bitblast_simplification = 0;
-  // If the bit-blaster discovers new constants, should the term simplifier be
-  // re-run.
-  bool simplify_during_BB_flag = false;
   // given a/b = c, propagates that c<=a even if b may be zero.
   bool cBitP_propagateForDivisionByZero = true;
-  bool traditional_cnf = false;
-  bool simple_cnf = false; // don't use the good AIG based CNF conversion.
+
+  bool array_difficulty_reversion = true;
+  bool difficulty_reversion = true;
+
 
   // eagerly write through the array's function congruence axioms.
   bool ackermannisation = false;
-
-  // check the counterexample against the original input to STP
-  bool check_counterexample_flag = false;
 
   // construct the counterexample in terms of original variable based
   // on the counterexample returned by SAT solver
   bool print_counterexample_flag = false;
   bool print_binary_flag = false;
-
-  //This is derived from other settings.
-  bool construct_counterexample_flag = false;
 
   // if this option is true then print the way dawson wants using a
   // different printer. do not use this printer.
@@ -110,23 +96,14 @@ public:
   bool print_STPinput_back_dot_flag = false;
   bool print_STPinput_back_GDL_flag = false;
 
-  bool array_difficulty_reversion = true;
-  bool difficulty_reversion = true;
+  bool print_nodes_flag = false;
+
 
   // output flags
   bool output_CNF_flag = false;
   bool output_bench_flag = false;
 
-  // Flag to switch on the smtlib parser
-  bool smtlib1_parser_flag = false;
-  bool smtlib2_parser_flag = false;
-
-  bool quick_statistics_flag = false;
-
-  bool exit_after_CNF = false;
-
-  int num_solver_threads = 1;
-
+  /* Bitblasting options */
 
   // You can select these with any combination you want of true & false.
   bool division_variant_1 = true;
@@ -139,6 +116,28 @@ public:
   bool conjoin_to_top = true;
 
   int64_t multiplication_variant = 7;
+
+  // If the bit-blaster discovers new constants, should the term simplifier be
+  // re-run.
+  bool simplify_during_BB_flag = false;
+
+
+  /* CNF Generation options */
+  bool traditional_cnf = false;
+  bool simple_cnf = false; // don't use the good AIG based CNF conversion.
+
+  bool exit_after_CNF = false;
+
+  /* SAT solving options */
+
+  int64_t timeout_max_conflicts = -1;
+  int num_solver_threads = 1;
+  int64_t timeout_max_time = -1; // seconds
+
+  // check the counterexample against the original input to STP
+  bool check_counterexample_flag = false;
+  //This is derived from other settings.
+  bool construct_counterexample_flag = false;
 
 
 
