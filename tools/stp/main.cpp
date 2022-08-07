70e11b3bb589197456b5f18f0e4c657e1fcd455a
Trevor Hansen <trev_abroad@yahoo.com>
2022-08-07 14:59:20 +1000

    Update main.cpp
    oops. I'm using the web interface for the first time and getting all confused.

--- tools/stp/main.cpp
+++ tools/stp/main.cpp
@@ -38,10 +38,7 @@ using std::endl;
  * step 0. Parse the input into an ASTVec.
  * step 1. Do BV Rewrites
  * step 2. Bitblasts the ASTNode.
- 
- Transfer issue
- Convert to discussion
- Delete issue * step 3. Convert to CNF
+ * step 3. Convert to CNF
  * step 4. Convert to SAT
  * step 5. Call SAT to determine if input is SAT or UNSAT
 ********************************************************************/
@@ -281,9 +278,6 @@ void ExtraMain::create_options()
       "print dotty/neato's graph format, then exit")(
       "print-counterex,p",
       po::bool_switch(&(bm->UserFlags.print_counterexample_flag)), 
- Transfer issue
- Convert to discussion
- Delete issue
       "print counterexample")(
       "print-counterexbin,y",
       po::bool_switch(&(bm->UserFlags.print_binary_flag)),
