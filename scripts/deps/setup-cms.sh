04a8abebb8876166f1031f6da04cea2cac28060f
Andrew V. Teylu <andrew@tey.lu>
2024-07-13 22:18:13 +0100

    Move to CMS @ 5.11.22

--- scripts/deps/setup-cms.sh
+++ scripts/deps/setup-cms.sh
@@ -31,8 +31,8 @@ cd ..
 
 git clone https://github.com/msoos/cryptominisat "${dep}"
 cd "${dep}"
-# We specify the commits for the other repositories, so do for this too
-git checkout ffcf4e0
+# We specify the tags/commits for the other repositories, so do for this too
+git checkout 5.11.22
 mkdir build && cd build
 cmake -DENABLE_ASSERTIONS=OFF -DCMAKE_INSTALL_PREFIX:PATH="${install_dir}" ..
 cmake --build . --parallel "$(nproc)"
